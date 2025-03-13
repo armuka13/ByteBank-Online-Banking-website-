b097fbe7581d8e862568970d1f2cfb15eb164795
TheButcher13 <muka.arjan@outlook.com>
2025-03-14 00:09:24 +0100

    Created Login/Register forms Connected Login/Register forms to main page and each one will open respectively LogIn and Register are on the same page, distinguished by div-s, where one is shown and the other is hidden with JS

--- script.js
+++ script.js
@@ -1,11 +1,38 @@
-//Shikoje kete funksion per te shfaqur ose fshire navbarin
-    function toggleNavbar() {
-        if (window.innerWidth < 768) {
-            document.getElementById("myNavbar").style.display = "none";
-        } else {
-            document.getElementById("myNavbar").style.display = "block";
-        }
+// //Shikoje kete funksion per te shfaqur ose fshire navbarin
+//     function toggleNavbar() {
+//         if (window.innerWidth < 768) {
+//             document.getElementById("myNavbar").style.display = "none";
+//         } else {
+//             document.getElementById("myNavbar").style.display = "block";
+//         }
+//     }
+
+//     window.addEventListener("resize", toggleNavbar);
+//     toggleNavbar(); // Run on page load
+
+    //gets from the main page Sign Up which reads register and Log In which reads login
+
+    var formType = new URLSearchParams(window.location.search).get('formType');
+
+    //Checks if it is register of login
+    if (formType === 'register') {
+        $("#signInForm").hide();
+        $("#registerForm").show();
+    } else if (formType === 'login') {
+        $("#registerForm").hide();
+        $("#signInForm").show();
     }
 
-    window.addEventListener("resize", toggleNavbar);
-    toggleNavbar(); // Run on page load
+//When clicking on Sign In or Register it will hide the other form
+$("#signInTxt").click(function() {
+    $("#registerForm").hide();
+    $("#signInForm").show();
+})
+
+$("#registerTxt").click(function() {
+    $("#signInForm").hide();
+    $("#registerForm").show();
+})
+
+
+
